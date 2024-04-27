3b31e8cad61cbe90910c48a8531edc048ae83f3b
ForgeDom <maxstill2006@gmail.com>
2024-04-27 16:45:12 +0300

    Homework ex3 done

--- main.cpp
+++ main.cpp
@@ -7,5 +7,18 @@ int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	
+	cout << "Введіть 7 чисел по черзі" << endl;
+	int numbers[7];
+	for (int i = 0; i < 7; i++) {
+		cout << "Введіть ціле число:" << endl;
+		cin >> numbers[i];
+	}
+	int max_number = numbers[0];
+	for (int i = 1; i < 7; i++) {
+		if (numbers[i] > max_number) {
+			max_number = numbers[i];
+		}
+	}
+	cout << "Максимальне число: " << max_number;
 	return 0;
 }

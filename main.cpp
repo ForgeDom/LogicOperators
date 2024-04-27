67d9ad75c10a695aba8c586f9c874248e6b0c8b8
ForgeDom <maxstill2006@gmail.com>
2024-04-27 15:44:48 +0300

    Lab ex3 done

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ using namespace std;
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	
+	int x;
+	cin >> x;
+	string res = (x > 0) ? "Додатнє" : (x < 0) ? "Від'ємне": "Дорінює нулю";
+	cout << res;
 	return 0;
 }

d4db4823210352cd3e023fc08bd76c585e313c96
ForgeDom <maxstill2006@gmail.com>
2024-04-27 16:01:20 +0300

    Lab ex4 done

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <cmath>
 #include <Windows.h>
+#include <String>
 using namespace std;
 
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	
+
+    int x,y;
+	cin >> x >> y;
+
+	string res = (x == y) ? "×èñëà ð³âí³" : (x > y) ? to_string(y) + " " + to_string(x)  : to_string(x) + " " + to_string(y);
+	cout << res;
 	return 0;
 }

d806559b0877a16d089fcecd0771929d96801c29
ForgeDom <maxstill2006@gmail.com>
2024-04-27 16:56:15 +0300

    rework

--- main.cpp
+++ main.cpp
@@ -1,24 +1,39 @@
 #include <iostream>
 #include <cmath>
 #include <Windows.h>
+
 using namespace std;
 
 int main() {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	
-	cout << "Введіть 7 чисел по черзі" << endl;
-	int numbers[7];
-	for (int i = 0; i < 7; i++) {
-		cout << "Введіть ціле число:" << endl;
-		cin >> numbers[i];
-	}
-	int max_number = numbers[0];
-	for (int i = 1; i < 7; i++) {
-		if (numbers[i] > max_number) {
-			max_number = numbers[i];
-		}
-	}
-	cout << "Максимальне число: " << max_number;
-	return 0;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Введіть сім цілих чисел:" << endl;;
+
+    int num1, num2, num3, num4, num5, num6, num7;
+    cin >> num1 >> num2 >> num3 >> num4 >> num5 >> num6 >> num7;
+
+    int max_number = num1;
+    if (num2 > max_number) {
+        max_number = num2;
+    }
+    if (num3 > max_number) {
+        max_number = num3;
+    }
+    if (num4 > max_number) {
+        max_number = num4;
+    }
+    if (num5 > max_number) {
+        max_number = num5;
+    }
+    if (num6 > max_number) {
+        max_number = num6;
+    }
+    if (num7 > max_number) {
+        max_number = num7;
+    }
+
+    cout << "Максимальне число: " << max_number << endl;
+
+    return 0;
 }
